eb86f917bd3c70060a76cbd7f36cd94510fee685
asmithasatesh <asmithasatesh@gmail.com>
2021-07-24 11:41:37 +0530

    [Asmitha]Perform Dictionary Operations

--- CollectionDemo/Program.cs
+++ CollectionDemo/Program.cs
@@ -11,6 +11,7 @@ namespace CollectionDemo
             Console.WriteLine("Enter 2- for HashSet Implementation");
             Console.WriteLine("Enter 3- for Queue Implementation");
             Console.WriteLine("Enter 4- for Stack Implementation");
+            Console.WriteLine("Enter 5- for Dictionary Implementation");
             //Get option from user
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -26,6 +27,9 @@ namespace CollectionDemo
                     break;
                 case 4:
                     Stack.StackOperations();
+                    break;
+                case 5:
+                    Dictionary.DictionaryOperations();
                     break;
                 default:
                     break;
