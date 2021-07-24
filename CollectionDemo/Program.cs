61f493168203efe74bee15c178ba7c6b07b15003
asmithasatesh <asmithasatesh@gmail.com>
2021-07-24 10:59:59 +0530

    [Asmitha]Perform List Operations

--- CollectionDemo/Program.cs
+++ CollectionDemo/Program.cs
@@ -7,6 +7,17 @@ namespace CollectionDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Collection demo!");
+            Console.WriteLine("Enter 1- for List Implementation");
+            //Get option from user
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    List.ListOperations();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
